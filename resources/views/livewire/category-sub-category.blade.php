f32dfb8f7be914727d0149598f8635f4214987c5
MyDy-003-Nirupa <mydynamica03@outlook.com>
2025-03-15 08:53:42 +0530

    update

--- resources/views/livewire/category-sub-category.blade.php
+++ resources/views/livewire/category-sub-category.blade.php
@@ -1,15 +1,19 @@
 <div>
     {{-- The best athlete wants his opponent at his best. --}}
 
-    <p>Our category sub category Livewire componenet </p>
 
-    <select class="form-control" wire:model.live="selectedCategory">
+
+    <label for="category_id" class="fw-bold mb-2">Select a category for your Product</label>
+    <select class="form-control mb-2" name="category_id" wire:model.live="selectedCategory">
         	<option value=" ">Select a category </option>
         	@foreach ($categories as $category)
                 <option value="{{$category->id}}">{{$category->category_name}}</option>
             @endforeach
     </select>
-    <select class="form-control">
+
+    <label for="subcategory_id" class="fw-bold mb-2">Select a sub category for your Product</label>
+
+    <select class="form-control mb-2" name="subcategory_id">
         <option value=" ">Select Sub category </option>
         @foreach ($subcategories as $subcategory)
             <option value="{{$subcategory->id}}">{{$subcategory->subcategory_name}}</option>
